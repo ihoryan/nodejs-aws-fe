1443bfff377a1c71325912ec157faf167717d849
ihorjan <igor@MacBook-Pro-igor.local>
2020-10-24 18:36:39 +0300

    Revert "perform hw-2"
    This reverts commit 65287faddf18a74e30cf85dfeb114c3bae280d70.

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,9 +8,9 @@ import Header from "components/MainLayout/components/Header";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright RS School © '}
+      {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        RS Store
+        My Store
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
