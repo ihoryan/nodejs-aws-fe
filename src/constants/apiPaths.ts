8df7704edfcd9f06449078545c19d649b9bf295c
ihorjan <igor@MacBook-Pro-igor.local>
2020-10-31 13:02:46 +0200

    hw-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://nvm9mjawcl.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://nvm9mjawcl.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://nvm9mjawcl.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://nvm9mjawcl.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
